b591cc31e21111d840ab9d8a8fad416795d9a8d6
matewilk <matewilk@gmail.com>
2023-02-20 12:36:06 +0000

    (feat): update browser agent ts types

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,8 +1,16 @@
 import type newrelic from "newrelic";
 
+interface Newrelic extends newrelic {
+  noticeError(
+    error: (Error & { statusCode?: number | undefined }) | null | undefined
+  ): void;
+  addPageAction: (actionName: string, customAttributes?: object) => void;
+  setCustomAttribute: (key: string, value: string | number) => void;
+}
+
 // for the New Relic browser agent
 declare global {
   interface Window {
-    newrelic: newrelic;
+    newrelic: Newrelic;
   }
 }
